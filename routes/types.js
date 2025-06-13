6eabb192a6812e0f450485cfd65792a72cc77450
glasters <glandhf32@gmail.com>
2025-06-13 21:15:27 +0300

    only working

--- routes/types.js
+++ routes/types.js
@@ -3,7 +3,7 @@ const pool = require('../models/database');
 const router = express.Router();
 
 
-// Получить все снаряжение
+// Получить типы снаряжения
 router.get('/', async (req, res) => {
     try {
         const [types] = await pool.execute(`
@@ -17,10 +17,6 @@ router.get('/', async (req, res) => {
         console.error('Ошибка получения типов снаряжения:', error);
         res.status(500).json({ message: 'Ошибка загрузки типов снаряжения: ' + error.message });
     }
-});
-// Получить типы снаряжения
-router.get('/', async (req, res) => {
-    
 });
 
 module.exports = router;
