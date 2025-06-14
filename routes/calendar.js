5664af7be56656e26cfadcd2c5d0cdc2ba4dc004
glasters <glandhf32@gmail.com>
2025-06-14 22:51:51 +0300

    calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -9,13 +9,26 @@ router.get('/:id/:datas/:datapo', async (req, res) => {
         const { id } = req.params;
         
         const [equipment] = await pool.execute(`
-            SELECT 
-                VidSn.id_vid,
-                
-            FROM DvSnar 
-            LEFT JOIN VidSn ON VidSn.id_vid = DvSnar.id_vid
-            LEFT JOIN Zajav ON Zajav.id_zajav=DvSnar.id_zajav 
-        `, [id]);
+            WITH RECURSIVE DateRange AS (
+    SELECT '2025-05-26' AS Date
+    UNION ALL
+    SELECT DATE_ADD(Date, INTERVAL 1 DAY)
+    FROM DateRange
+    WHERE Date < '2025-07-06'
+)
+SELECT 
+Zajav.id_zajav, DvSnar.id_vid, \`Date\`,
+VidSn.kolich as kol_all,Zajav.datas,Zajav.datapo,
+DvSnar.kolvo as kol_z, Zajav.id_status, 
+(VidSn.kolich-DvSnar.kolvo) as raznica
+FROM Zajav, DateRange,DvSnar,VidSn
+WHERE DateRange.Date BETWEEN ? AND ?
+AND   DateRange.Date BETWEEN Zajav.datas AND Zajav.datapo
+AND Zajav.id_zajav=DvSnar.id_zajav
+AND VidSn.id_vid = DvSnar.id_vid
+AND VidSn.id_vid = ?
+ORDER BY \`Date\` 
+        `, [datas,datapo,id]);
 
         if (equipment.length === 0) {
             return res.status(404).json({ message: 'Снаряжение не найдено' });
