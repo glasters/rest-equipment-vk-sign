9cc227e4f010dd2a900a7aadc1ba11b09f272251
glasters <glandhf32@gmail.com>
2025-06-14 16:47:54 +0300

    pr

--- routes/print.js
+++ routes/print.js
@@ -3,7 +3,33 @@ const pool = require("../models/database");
 const router = express.Router();
 
 // Получить по ID
-router.get("/:id", async (req, res) => {
+router.get("/lessor/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [equipment] = await pool.execute(
+      `
+            SELECT id_zajav , ds.id_vid , ds.kolvo, vs.vnaim,vs.zenapr,vs.zenapr
+            FROM DvSnar ds 
+            LEFT JOIN VidSn vs  ON vs.id_vid = ds.id_vid 
+            WHERE ds.id_vid = 3
+        `,
+      [id]
+    );
+
+    if (equipment.length === 0) {
+      return res.status(404).json({ message: "Снаряжение не найдено" });
+    }
+
+    res.json(equipment[0]);
+  } catch (error) {
+    console.error("Ошибка получения снаряжения:", error);
+    res
+      .status(500)
+      .json({ message: "Ошибка загрузки снаряжения: " + error.message });
+  }
+});
+router.get("/lessee/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
